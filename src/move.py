6dc34ac3ba4317c90c44e2a28e997c316031f87b
SamuelPinaLopes <samuelpinalopesws@gmail.com>
2026-06-18 10:38:08 -0100

    understanding argparse and catching command arguments

--- src/move.py
+++ src/move.py
@@ -1,7 +1,5 @@
 import argparse
 
-
-
 parse = argparse.ArgumentParser(description="Command to copy several folders at once.")
 
 parse.add_argument("-v", "--verbose", action="store_true", help="see what is going on while moving")
@@ -10,15 +8,11 @@ parse.add_argument("folder", nargs="+", help="input your folders")
 
 args = parse.parse_args()
 
-
 if args.verbose == True:
     for folders in args.folder:
         print(folders)
     print("\n\ndestination of folders: ", args.dest)
     print("verbose flag added: ", args.verbose)
 
-
-
 else:
     print("you don't have to see anything if you didn't put the flag...\n\n:<")
-
